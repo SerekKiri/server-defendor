038aab3627dd08115094ba1c5bb47fc3fecf2c89
rnickson <mickson@tuta.io>
2018-05-11 23:06:45 +0200

    :tada: Removing messages with forbidden phrase/emote like :joy: or 'Xd'

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const debug = require('debug')('index');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+
+if (process.env.NODE_ENV === 'production' || process.env.BOT_TOKEN !== undefined) {
+  client.login(process.env.BOT_TOKEN);
+} else {
+  client.login(config.token);
+}
+
+client.on('ready', () => {
+  debug(`Defendor has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setPresence({ game: { name: 'spammers\' screams', type: 2 } });
+});
+
+client.on('message', async (message) => {
+  let deleted = false;
+  let i = 0;
+  let content = message.content.toLowerCase();
+  debug(content);
+  for (; i < config.forbidden.caseInsensitive.length; i += 1) {
+    if (content.includes(config.forbidden.caseInsensitive[i].toLowerCase())) {
+      message.delete(1000);
+      debug(`Caught insensitive ${message.content}`);
+      deleted = true;
+      break;
+    }
+  }
+  if (deleted === false) {
+    i = 0;
+    content = message.content;
+    for (; i < config.forbidden.caseSensitive.length; i += 1) {
+      if (content.includes(config.forbidden.caseSensitive[i])) {
+        message.delete(1000);
+        debug(`Caught sensitive ${message.content}`);
+        deleted = true;
+        break;
+      }
+    }
+  }
+});
