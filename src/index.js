4dda74fe1c2dca5930b10477ed9959756222216b
rnickson <mickson@tuta.io>
2018-05-11 23:50:06 +0200

    :sparkles: Assigning `Użytkownik` role to new users

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ client.on('message', async (message) => {
   let deleted = false;
   let i = 0;
   let content = message.content.toLowerCase();
-  debug(content);
   for (; i < config.forbidden.caseInsensitive.length; i += 1) {
     if (content.includes(config.forbidden.caseInsensitive[i].toLowerCase())) {
       message.delete(1000);
@@ -40,4 +39,10 @@ client.on('message', async (message) => {
       }
     }
   }
+});
+
+client.on('guildMemberAdd', (member) => {
+  debug(`User ${member.user.username} joined`);
+  const role = member.guild.roles.find('name', 'Użytkownik');
+  member.addRole(role).catch(debug);
 });
