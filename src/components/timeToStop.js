a3885b7377c1e17769208141f0f6f00ddfb358fa
Unknown <kacper.potyrala@gmail.com>
2018-05-12 10:54:41 +0200

    :lipstick: More friendly color for message

--- src/components/timeToStop.js
+++ src/components/timeToStop.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js');
 
 module.exports = new Discord.RichEmbed()
   .setTitle('**IT\'S TIME TO STOP**')
-  .setColor(0xea5353)
+  .setColor(0xcc0000)
   .setImage('https://imgur.com/RniBbFr.png');
